0bcae78cfea48e7efd60b09889f581c6bef64179
SatzzDev <94371633+SatzzDev@users.noreply.github.com>
2024-07-10 01:10:24 +0700

    Fix Welcome & GoodBye Image

--- routes/api.js
+++ routes/api.js
@@ -45,15 +45,7 @@ return res.data;
 return err;
 }
 };
-const getBuffer = async (url, options) => { 
-try { 
-options ? options : {} 
-const res = await axios({ method: "get", url, headers: { 'DNT': 1, 'Upgrade-Insecure-Request': 1 }, ...options, responseType: 'arraybuffer' }) 
-return res.data 
-} catch (err) { 
-return err 
-}
-}
+
 
 async function fetchAsupanVideo() {
 while (true) {
